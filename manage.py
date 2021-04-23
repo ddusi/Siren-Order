93191cb14f17fe907dffc2cd9515a32e9200ecbc
ddusi <ddusi@kakao.com>
2021-04-23 21:26:00 +0900

    manage.py에서 APPS 디렉토리 인식 못하는 버그 수정

--- manage.py
+++ manage.py
@@ -5,7 +5,9 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administratㅓive tasks."""
+    sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "apps"))
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     try:
         from django.core.management import execute_from_command_line
